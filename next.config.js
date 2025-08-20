e5c3e049c6b17b4e7c9c50aaaaf32bf6a4521c49
antonio.kudric <antonio4.kudric@gmail.com>
2025-08-20 23:07:49 +0200

    Changed config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'ethical-champion-a6f0cd3e63.strapiapp.com' },
-      // add your CDN/asset hosts here later if needed
-    ],
-  },
-};
-export default nextConfig;
+const nextConfig = {};
+
+module.exports = nextConfig;
